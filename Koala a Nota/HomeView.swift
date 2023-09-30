337c57eebc9b0c2292e81afc2a6e83f94d95d639
thiago-you <thiago.youx@gmail.com>
2023-09-30 18:14:30 -0300

    add home view

--- Koala a Nota/HomeView.swift
+++ Koala a Nota/HomeView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct HomeView: View {
+    let dataModel = DataModel()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(showsIndicators: false) {
+            headerView
+            listView
+        }
+    }
+
+    var headerView: some View {
+        VStack {
+            Image("logo")
+                .resizable()
+                .frame(width: 50, height: 50, alignment: .trailing)
+        }
+        .frame(height: 240)
+    }
+
+    var listView: some View {
+        VStack {
+            VStack {
+                Text("Avaliações")
+                    .font(.title.bold())
+                    .frame(maxWidth: .infinity)
+                    .padding(.top, 5)
+                    .padding(.bottom, 5)
+                ForEach(dataModel.reviews) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.title)
+                                .font(.title2)
+                                .bold()
+                            Text(item.review)
+                        }
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(.white)
+                }
+            }
+        }
+        .padding()
+        .background(Color.white)
     }
 }
 
