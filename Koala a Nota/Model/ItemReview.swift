dbd6201669514806c02685afd57c61422d09a973
thiago-you <thiago.youx@gmail.com>
2023-09-30 17:17:56 -0400

    add model

---
+++ Koala a Nota/Model/ItemReview.swift
@@ -0,0 +1,8 @@
+//
+//  ItemReview.swift
+//  Koala a Nota
+//
+//  Created by user241334 on 9/30/23.
+//
+
+import Foundation
---
+++ Koala a Nota/Model/ItemReviewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ItemReviewModel.swift
+//  Koala a Nota
+//
+//  Created by user241334 on 9/30/23.
+//
+
+import Foundation
