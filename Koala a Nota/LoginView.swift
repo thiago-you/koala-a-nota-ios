c283d4f08198df845e09edc599acf922565a7838
thiago-you <thiago.youx@gmail.com>
2023-09-30 17:59:15 -0300

    add login

--- Koala a Nota/LoginView.swift
+++ Koala a Nota/LoginView.swift
@@ -8,8 +8,85 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var senha = ""
+    @State private var rememberPassword = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { reader in
+            ZStack {
+                VStack {
+                    Spacer()
+                    Image("logo")
+                        .resizable()
+                        .frame(width: 250, height: 250)
+                    Spacer()
+                    ZStack {
+                        Color.white.ignoresSafeArea()
+                        VStack {
+                            Text("Entre com seu Login")
+                                .font(.title)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(.bottom, 30)
+                            Text("E-mail:")
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            TextField("Digite seu e-mail...", text: $email)
+                                .padding(.all)
+                                .overlay(RoundedRectangle(cornerRadius: 8.0).strokeBorder(Color("lightPurple"), style: StrokeStyle(lineWidth: 1.0)))
+                                .padding(.bottom, 20)
+                            HStack {
+                                Text("Senha:")
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                Spacer()
+                                Button("Recuperar Senha") {
+                                    
+                                }
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                            }
+                            SecureField("Digite sua senha...", text: $senha)
+                                .padding(.all)
+                                .overlay(RoundedRectangle(cornerRadius: 8.0).strokeBorder(Color("lightPurple"), style: StrokeStyle(lineWidth: 1.0)))
+                                .padding(.bottom, 20)
+                            HStack {
+                                Text("Lembrar minha senha")
+                                    .font(.caption)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                Spacer()
+                                Toggle("", isOn: $rememberPassword)
+                            }
+                            .padding(.bottom, 30)
+                            Button("Entrar") {
+                                
+                            }
+                            .frame(maxWidth: .infinity)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color("darkerPurple"))
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                            .padding(.bottom, 10)
+                            Button(action: {}) {
+                                HStack {
+                                    Text("Não possui uma conta?")
+                                        .font(.caption)
+                                        .foregroundColor(.black)
+                                    Text("Crie")
+                                        .font(.callout)
+                                        .foregroundColor(.blue)
+                                }
+                            }
+                            .padding(.bottom, 10)
+                        }
+                        .padding()
+                    }
+                    .fixedSize(horizontal: false, vertical: true)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                }
+                .frame(minHeight: reader.size.height)
+                .background(
+                    LinearGradient(gradient: Gradient(colors: [Color("darkerPurple"), Color("darkPurple"), .white]), startPoint: .top, endPoint: .bottom)
+                )
+            }
+        }
     }
 }
 
--- Koala a Nota/SignupView.swift
+++ Koala a Nota/SignupView.swift
@@ -8,86 +8,86 @@
 import SwiftUI
 
 struct SignupView: View {
-    @State private var email = "thiago.you@puc.com.br"
-        @State private var senha = "thiago.you"
-        @State private var rememberPassword = true
-        
-        var body: some View {
-            GeometryReader { reader in
-                ZStack {
-                    VStack {
-                        Spacer()
-                        Image("logo")
-                            .resizable()
-                            .frame(width: 250, height: 250)
-                        Spacer()
-                        ZStack {
-                            Color.white.ignoresSafeArea()
-                            VStack {
-                                Text("Crie sua conta")
-                                    .font(.title)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .padding(.bottom, 30)
-                                Text("E-mail:")
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                TextField("Digite seu e-mail...", text: $email)
-                                    .padding(.all)
-                                    .overlay(RoundedRectangle(cornerRadius: 8.0).strokeBorder(Color("lightPurple"), style: StrokeStyle(lineWidth: 1.0)))
-                                    .padding(.bottom, 20)
-                                HStack {
-                                    Text("Senha:")
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                    Spacer()
-                                    Button("Recuperar Senha") {
-                                        
-                                    }
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                                }
-                                SecureField("Digite sua senha...", text: $senha)
-                                    .padding(.all)
-                                    .overlay(RoundedRectangle(cornerRadius: 8.0).strokeBorder(Color("lightPurple"), style: StrokeStyle(lineWidth: 1.0)))
-                                    .padding(.bottom, 20)
-                                HStack {
-                                    Text("Lembrar minha senha")
-                                        .font(.caption)
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                    Spacer()
-                                    Toggle("", isOn: $rememberPassword)
-                                }
+    @State private var email = ""
+    @State private var senha = ""
+    @State private var rememberPassword = true
+    
+    var body: some View {
+        GeometryReader { reader in
+            ZStack {
+                VStack {
+                    Spacer()
+                    Image("logo")
+                        .resizable()
+                        .frame(width: 250, height: 250)
+                    Spacer()
+                    ZStack {
+                        Color.white.ignoresSafeArea()
+                        VStack {
+                            Text("Crie sua conta")
+                                .font(.title)
+                                .frame(maxWidth: .infinity, alignment: .leading)
                                 .padding(.bottom, 30)
-                                Button("Criar conta") {
+                            Text("E-mail:")
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            TextField("Digite seu e-mail...", text: $email)
+                                .padding(.all)
+                                .overlay(RoundedRectangle(cornerRadius: 8.0).strokeBorder(Color("lightPurple"), style: StrokeStyle(lineWidth: 1.0)))
+                                .padding(.bottom, 20)
+                            HStack {
+                                Text("Senha:")
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                Spacer()
+                                Button("Recuperar Senha") {
                                     
                                 }
-                                .frame(maxWidth: .infinity)
-                                .foregroundColor(.white)
-                                .padding()
-                                .background(Color("darkerPurple"))
-                                .clipShape(RoundedRectangle(cornerRadius: 8))
-                                .padding(.bottom, 10)
-                                Button(action: {}) {
-                                    HStack {
-                                        Text("Ja possui uma conta?")
-                                            .font(.caption)
-                                            .foregroundColor(.black)
-                                        Text("Entre")
-                                            .font(.callout)
-                                            .foregroundColor(.blue)
-                                    }
-                                }
-                                .padding(.bottom, 10)
+                                .frame(maxWidth: .infinity, alignment: .trailing)
                             }
+                            SecureField("Digite sua senha...", text: $senha)
+                                .padding(.all)
+                                .overlay(RoundedRectangle(cornerRadius: 8.0).strokeBorder(Color("lightPurple"), style: StrokeStyle(lineWidth: 1.0)))
+                                .padding(.bottom, 20)
+                            HStack {
+                                Text("Lembrar minha senha")
+                                    .font(.caption)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                Spacer()
+                                Toggle("", isOn: $rememberPassword)
+                            }
+                            .padding(.bottom, 30)
+                            Button("Criar conta") {
+                                
+                            }
+                            .frame(maxWidth: .infinity)
+                            .foregroundColor(.white)
                             .padding()
+                            .background(Color("darkerPurple"))
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                            .padding(.bottom, 10)
+                            Button(action: {}) {
+                                HStack {
+                                    Text("Ja possui uma conta?")
+                                        .font(.caption)
+                                        .foregroundColor(.black)
+                                    Text("Entre")
+                                        .font(.callout)
+                                        .foregroundColor(.blue)
+                                }
+                            }
+                            .padding(.bottom, 10)
                         }
-                        .fixedSize(horizontal: false, vertical: true)
-                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                        .padding()
                     }
-                    .frame(minHeight: reader.size.height)
-                    .background(
-                        LinearGradient(gradient: Gradient(colors: [Color("darkerPurple"), Color("darkPurple"), .white]), startPoint: .top, endPoint: .bottom)
-                    )
+                    .fixedSize(horizontal: false, vertical: true)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
                 }
+                .frame(minHeight: reader.size.height)
+                .background(
+                    LinearGradient(gradient: Gradient(colors: [Color("darkerPurple"), Color("darkPurple"), .white]), startPoint: .top, endPoint: .bottom)
+                )
             }
         }
+    }
 }
 
 struct Signup_Previews: PreviewProvider {
